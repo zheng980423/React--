b898cc37af2fe0a0e37b924beb2cd5baae2796f2
zwf <992684658@qq.com>
2020-06-07 19:56:23 +0800

    初始配饰和样式

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const Game = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="grid">
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+        <div className="cell" style={{ width: "33.3%" }} />
+      </div>
+      <div className="message">Game Message Here...</div>
+      <div className="button">
+        <button>Start Game</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Game;
